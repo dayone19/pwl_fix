dd2fd6dc852bc84323072fa64b5158be98fc2b11
dayone19 <deyoansalsabila43@gmail.com>
2026-05-05 20:52:49 +0700

    tes gabung root

--- tubes_payroll/routes/web.php
+++ tubes_payroll/routes/web.php
@@ -1,10 +1,3 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-//dashboard
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
