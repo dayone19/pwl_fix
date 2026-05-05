67f0dbfca3a8d5cfc52cafb9b8e8bf1224b43810
dayone19 <deyoansalsabila43@gmail.com>
2026-05-05 20:51:00 +0700

    Simpan perubahan sementara sebelum pindah ke Deyoan

--- tubes_payroll/routes/web.php
+++ tubes_payroll/routes/web.php
@@ -2,6 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 
+//landingPage
 Route::get('/', function () {
-    return view('welcome');
+    return view('landingPage'); 
 });
+
+//Login
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [LoginController::class, 'login']);
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+
+Route::get('/register', function () {
+    return view('auth.register');
+})->name('register');
+
+//dashboard
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
