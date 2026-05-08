ed561e0cedbcf1d56a690ee4bb371f22177ec101
qeizha111 <qeizzyva@gmail.com>
2026-05-08 12:19:12 +0700

    revisi qeizha

--- tubes_payroll/routes/web.php
+++ tubes_payroll/routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\absensiController;
+use App\Http\Controllers\karyawanController;
 
 //landingPage
 Route::get('/', function () {
